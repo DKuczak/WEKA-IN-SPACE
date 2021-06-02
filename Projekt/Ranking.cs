8f65ba855418f2b8d27c08858d3ab4e18f71a173
DKuczak <259513@student.pwr.edu.pl>
2021-06-02 13:29:14 +0200

    Poprawa wyglądu kodu

--- Projekt/Ranking.cs
+++ Projekt/Ranking.cs
@@ -24,33 +24,30 @@ namespace Projekt
         {
             string drogaDoPliku = "ranking.txt";
             StreamWriter plik;
+
             // sprawdzenie czy plik istnieje
-            if (!File.Exists(drogaDoPliku))
-            {
+            if (!File.Exists(drogaDoPliku)) {
                 //jeśli plik nie istnieje należy go stworzyć
                 plik = File.CreateText("ranking.txt");
             }
-            else
-            {
+            else {
                 plik = new StreamWriter(drogaDoPliku, true);
             }
 
             plik.WriteLine($"{wynik.nazwaGracza} {wynik.wynik}");           
             plik.Close();
-
         }
         public List<Ranking> odczytRankingu()
         {
             List<Ranking> listaWynikow = new List<Ranking>();
             string drogaDoPliku = "ranking.txt";
+            
             //sprawdzenie czy plik istnieje
-            if (!File.Exists(drogaDoPliku))
-            {
+            if (!File.Exists(drogaDoPliku)) {
                 //jeśli plik nie istnieje ranking jest pusty
                 listaWynikow = null;
             }
-            else
-            {
+            else {
                 string[] linie = File.ReadAllLines(drogaDoPliku, Encoding.UTF8);
 
                 for (int x = 0; x <linie.Length; x++)
@@ -62,15 +59,13 @@ namespace Projekt
                         nazwaGracza = obecnaLinia[0],
                         wynik = Convert.ToInt32(obecnaLinia[1]),
                     };
+
                     listaWynikow.Add(rankingObiekt);
                 }
-
                 listaWynikow.Sort((x, y) => x.wynik.CompareTo(y.wynik));
                 listaWynikow.Reverse();
             }
             return listaWynikow;
         }
-
-
     }
 }
